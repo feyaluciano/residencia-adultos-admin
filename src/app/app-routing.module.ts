2d5463712406842fc582bed1fa4793b232fd715a
Luciano <feyaluciano@gmail.com>
2022-02-15 23:35:56 -0300

    i added a comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes:Routes=[  
-  //CUANDO INGRESO A misutio.com.ar/ redirige a /auth/login  POR LO TANTO CARGA EL MODULO auth (ver este modulo y su routing)  
+const routes: Routes = [
+  //CUANDO INGRESO A misutio.com.ar/ redirige a /auth/login  POR LO TANTO CARGA EL MODULO auth (ver este modulo y su routing)
   {
     path: 'admin',
-    loadChildren: () => import('./features/admin/admin.module').then(
-      m => m.AdminModule
-      ),    
-  }]
+    loadChildren: () =>
+      import('./features/admin/admin.module').then((m) => m.AdminModule),
+    //   path: 'public',
+    // loadChildren: () => import('./features/public/public.module').then(
+    //   m => m.PublicModule
+    //   ),
+  },
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   declarations: [],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
