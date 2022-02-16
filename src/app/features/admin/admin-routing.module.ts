fb7ea65b8bdfff0ec9670f84789f60fa2e95e299
Luciano <feyaluciano@gmail.com>
2022-02-16 12:40:25 -0300

    i restructured some directrories, and i created some pages

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { AuthComponent } from '../auth/pages/page-auth/auth.component';
-import { DashboardComponent } from '../dashboard/pages/dashboard.component';
+import { DashboardComponent } from '../dashboard/pages/page-dashboard/dashboard.component';
 
 const routes: Routes = [
   {
